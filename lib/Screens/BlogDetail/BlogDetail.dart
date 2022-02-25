a056801709d26199aba2c605fa2b885bf49b1416
ManyuDhyani <manyudhyani@gmail.com>
2022-02-25 23:07:15 +0530

    Implemented the Blog detail fetch function

--- lib/Screens/BlogDetail/BlogDetail.dart
+++ lib/Screens/BlogDetail/BlogDetail.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hashstrix_app/Models/BlogModel.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class BlogDetail extends StatefulWidget {
   int id;
@@ -10,6 +13,43 @@ class BlogDetail extends StatefulWidget {
 }
 
 class _BlogDetailState extends State<BlogDetail> {
+  List<Blogs> BlogList = [];
+  void fetchBlogData async {
+    String baseURL = "https://hashstrix.com/api/blog/";
+    try{
+      var response = await http.get(Uri.parse(baseURL + widget.id.toString()));
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        data.forEach((Blog) {
+          Blogs i = Blogs(
+            id: Blog['id'],
+            Title: Blog['title'],
+            Overview: Blog['overview'],
+            Content: Blog['content'],
+            Thumbnail: Blog['thumbnail'],
+            Timestamp: Blog['timestamp'],
+            ProfilePicture: Blog['author']['profile_picture'],
+            Verified: Blog['author']['verified'],
+            Username: Blog['author']['user']['username'],
+          );
+          BlogList.add(i);
+        });
+        return BlogList;
+      } else {
+        return Future.error("Server Error");
+      }
+    }
+    catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  void initState() {
+    fetchBlogData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     String id = widget.id.toString();
